dba36d500d017c11af82e59b47b4b5de247eda83
Naman <87573198+NmnGit19@users.noreply.github.com>
2023-08-04 08:32:58 +0530

    changes

--- code/v0.2/src/pages/home/Home.js
+++ code/v0.2/src/pages/home/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 
 import Banner from "../../Components/Banner";
 import OffersForAgents from "../../Components/OffersForAgents";
@@ -8,7 +8,7 @@ import CollapsibleGroup from "../../Components/CollapsibleGroup";
 import Footer from "../../Components/Footer";
 import CircularProgressBar from "../../Components/CircularProgressBar";
 import NumberCounter from "../../Components/NumberCounter";
-import "./Home.css"
+import "./Home.css";
 const Home = () => {
   return (
     <div>
@@ -19,12 +19,8 @@ const Home = () => {
       <section className="perfect_layout sect_padding">
         <div className="container">
           <div className="section_title">
-            <h3>
-              APARTMENT PLAN
-            </h3>
-            <div class="section_title_effect">
-              APARTMENT PLAN
-            </div>
+            <h3>APARTMENT PLAN</h3>
+            <div class="section_title_effect">APARTMENT PLAN</div>
           </div>
           <div className="section_sub_titiel">
             <h4>The Perfect Layouts</h4>
@@ -33,14 +29,11 @@ const Home = () => {
             <Tabs>
               <TabList>
                 <Tab>STUDIO APARTMENT</Tab>
-                <Tab>
-                  PENTHOUSES</Tab>
-                <Tab>
-                  3 BEDROOMS</Tab>
-                <Tab>
-                  2 BEDROOMS</Tab>
-                <Tab>
-                  1 BEDROOM</Tab>
+                <Tab>PENTHOUSES</Tab>
+                <Tab>3 BEDROOMS</Tab>
+                <Tab>2 BEDROOMS</Tab>
+                <Tab>1 BEDROOM</Tab>
+                <Tab>1 KITCHEN</Tab>
               </TabList>
 
               <TabPanel>
@@ -60,7 +53,6 @@ const Home = () => {
               </TabPanel>
             </Tabs>
           </div>
-
         </div>
       </section>
       <CollapsibleGroup />
