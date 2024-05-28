08827258ad89f228ad514af85e0743bdfe8899ce
sanskarsolankigithub <sanskar@hyperclouddigital.com>
2024-05-28 07:56:02 +0530

    property_docs

--- code/v10/src/components/PropertyDocuments.jsx
+++ code/v10/src/components/PropertyDocuments.jsx
@@ -1,12 +1,10 @@
 import React, { useState, useRef } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams, Link } from "react-router-dom";
 import { useCollection } from "../hooks/useCollection";
-import Card from "@mui/material/Card";
-import CardMedia from "@mui/material/CardMedia";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
 import { projectFirestore, projectStorage } from "../firebase/config";
 import { useFirestore } from "../hooks/useFirestore";
+
+import './PropertyDocuments.scss'
 
 const PropertyDocuments = () => {
   const { propertyDocumentId } = useParams();
@@ -96,184 +94,187 @@ const PropertyDocuments = () => {
   };
 
   return (
-    <div style={{ marginTop: "100px" }}>
-      {!showAIForm && (
-        <div className="add_btn" onClick={handleShowAIForm}>
-          <div className="add_btn_inner">
-            <div className="add_icon">+</div>
-            <div className="ab_text">Add new property document</div>
+    <div className="top_header_pg pg_bg property_docs_pg">
+      <div className="page_spacing">
+        <div className="pg_header d-flex align-items-center justify-content-between">
+          <div className="left">
+            <h2 className="m22 mb-1">Property Documents</h2>
+            <h4 className="r16 light_black">Add new, show and download existing document </h4>
+          </div>
+          <div className="right">
+            {!showAIForm && (
+              <div className="theme_btn btn_fill" onClick={handleShowAIForm}>
+                Add document
+              </div>
+            )}
           </div>
         </div>
-      )}
-      {showAIForm && (
-        <section className="property_card_single add_aditional_form">
-          <div className="more_detail_card_inner relative">
-            <h2 className="card_title">Select any one document ID</h2>
-            <div className="aai_form">
-              <div className="row" style={{ rowGap: "18px" }}>
-                <div className="col-md-12">
-                  <div className="form_field">
-                    <div className="field_box theme_radio_new">
-                      <div className="theme_radio_container">
-                        <div className="radio_single">
-                          <input
-                            type="radio"
-                            name="aai_type"
-                            id="aadhar"
-                            value="Aadhar"
-                            onChange={handleRadioChange}
-                            checked={selectedIdType === "Aadhar"}
-                          />
-                          <label htmlFor="aadhar">Aadhar</label>
-                        </div>
-                        <div className="radio_single">
-                          <input
-                            type="radio"
-                            name="aai_type"
-                            id="passport"
-                            value="Passport"
-                            onChange={handleRadioChange}
-                            checked={selectedIdType === "Passport"}
-                          />
-                          <label htmlFor="passport">Passport</label>
-                        </div>
-                        <div className="radio_single">
-                          <input
-                            type="radio"
-                            name="aai_type"
-                            id="voterid"
-                            value="Voter Id"
-                            onChange={handleRadioChange}
-                            checked={selectedIdType === "Voter Id"}
-                          />
-                          <label htmlFor="voterid">Voter Id</label>
-                        </div>
-                        <div className="radio_single">
-                          <input
-                            type="radio"
-                            name="aai_type"
-                            id="drivinglicense"
-                            value="Driving Licence"
-                            onChange={handleRadioChange}
-                            checked={selectedIdType === "Driving Licence"}
-                          />
-                          <label htmlFor="drivinglicense">
-                            Driving Licence
-                          </label>
-                        </div>
-                        <div className="radio_single">
-                          <input
-                            type="radio"
-                            name="aai_type"
-                            id="pancard"
-                            value="Pan Card"
-                            onChange={handleRadioChange}
-                            checked={selectedIdType === "Pan Card"}
-                          />
-                          <label htmlFor="pancard">Pan Card</label>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div>
-                  <div>
-                    <input
-                      type="file"
-                      id="serviceimageInput"
-                      onChange={handleFileChange}
-                      ref={fileInputRef}
-                    />
-                  </div>
-                </div>
-                <div className="col-md-11">
-                  <div className="add_info_text">
-                    <div className="form_field">
-                      <div className="relative">
-                        <input
-                          type="text"
-                          value={idNumber}
-                          onChange={handleIdNumberChange}
-                          placeholder="Enter Document Id"
-                          required
-                        />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="row mt-3">
-              <div className="col-sm-1">
-                <div
-                  className="theme_btn btn_border text-center"
-                  onClick={isUploading ? null : handleShowAIForm}
-                >
-                  Cancel
-                </div>
-              </div>
-              <div className="col-sm-3">
-                <div
-                  className={`theme_btn btn_fill text-center ${
-                    isUploading ? "disabled" : ""
-                  }`}
-                  onClick={isUploading ? null : addPropertyDocuments}
-                >
-                  {isUploading ? "Uploading..." : "Save"}
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-      )}
 
-      {/* CODE FOR DOCUMENT CARD DESGIN */}
-      <div style={{ margin: "50px" }}>
-        {propertyDocument &&
-          propertyDocument.map((doc, index) => (
-            <Card key={index} sx={{ maxWidth: 345 }}>
-              <div className="d-flex align-items-center justify-content-between p-2">
-                <div>
-                  <h5>{doc.idType}</h5>
+        {showAIForm && (
+          <>
+            <div className="vg22">
+            </div>
+            <section className="my_big_card">
+              <h2 className="card_title">Select any one document ID</h2>
+              <div className="aai_form">
+                <div className="row" style={{ rowGap: "18px" }}>
+                  <div className="col-md-12">
+                    <div className="form_field">
+                      <div className="field_box theme_radio_new">
+                        <div className="theme_radio_container">
+                          <div className="radio_single">
+                            <input
+                              type="radio"
+                              name="aai_type"
+                              id="aadhar"
+                              value="Aadhar"
+                              onChange={handleRadioChange}
+                              checked={selectedIdType === "Aadhar"}
+                            />
+                            <label htmlFor="aadhar">Aadhar</label>
+                          </div>
+                          <div className="radio_single">
+                            <input
+                              type="radio"
+                              name="aai_type"
+                              id="passport"
+                              value="Passport"
+                              onChange={handleRadioChange}
+                              checked={selectedIdType === "Passport"}
+                            />
+                            <label htmlFor="passport">Passport</label>
+                          </div>
+                          <div className="radio_single">
+                            <input
+                              type="radio"
+                              name="aai_type"
+                              id="voterid"
+                              value="Voter Id"
+                              onChange={handleRadioChange}
+                              checked={selectedIdType === "Voter Id"}
+                            />
+                            <label htmlFor="voterid">Voter Id</label>
+                          </div>
+                          <div className="radio_single">
+                            <input
+                              type="radio"
+                              name="aai_type"
+                              id="drivinglicense"
+                              value="Driving Licence"
+                              onChange={handleRadioChange}
+                              checked={selectedIdType === "Driving Licence"}
+                            />
+                            <label htmlFor="drivinglicense">
+                              Driving Licence
+                            </label>
+                          </div>
+                          <div className="radio_single">
+                            <input
+                              type="radio"
+                              name="aai_type"
+                              id="pancard"
+                              value="Pan Card"
+                              onChange={handleRadioChange}
+                              checked={selectedIdType === "Pan Card"}
+                            />
+                            <label htmlFor="pancard">Pan Card</label>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div>
+                    <div>
+                      <input
+                        type="file"
+                        id="serviceimageInput"
+                        onChange={handleFileChange}
+                        ref={fileInputRef}
+                      />
+                    </div>
+                  </div>
+                  <div className="col-md-11">
+                    <div className="add_info_text">
+                      <div className="form_field">
+                        <div className="relative">
+                          <input
+                            type="text"
+                            value={idNumber}
+                            onChange={handleIdNumberChange}
+                            placeholder="Enter Document Id"
+                            required
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-                <div>
-                  <span
-                    className="material-symbols-outlined"
-                    style={{ cursor: "pointer" }}
+              </div>
+              <div className="row mt-3">
+                <div className="col-sm-1">
+                  <div
+                    className="theme_btn btn_border text-center"
+                    onClick={isUploading ? null : handleShowAIForm}
                   >
-                    download
-                  </span>
+                    Cancel
+                  </div>
+                </div>
+                <div className="col-sm-3">
+                  <div
+                    className={`theme_btn btn_fill text-center ${isUploading ? "disabled" : ""
+                      }`}
+                    onClick={isUploading ? null : addPropertyDocuments}
+                  >
+                    {isUploading ? "Uploading..." : "Save"}
+                  </div>
                 </div>
               </div>
-              {doc.mediaType === "pdf" ? (
-                <iframe
-                  title="PDF Viewer"
-                  src={doc.documentUrl}
-                  style={{ width: "100%", height: "500px", border: "none" }}
-                ></iframe>
-              ) : (
-                <CardMedia
-                  component="img"
-                  height="194"
-                  image={doc.documentUrl || "https://via.placeholder.com/150"}
-                />
-              )}
-              <CardContent>
-                <Typography variant="body2" color="text.secondary">
-                  <h5>{doc.idType} Number :</h5>
-                  {doc.idNumber}
-                </Typography>
-              </CardContent>
-              <div onClick={() => deletePropertyDocument(doc.id)}>
-                <span
-                  className="material-symbols-outlined"
-                  style={{ cursor: "pointer" }}
-                >
-                  delete
-                </span>
-              </div>
-            </Card>
-          ))}
+            </section></>
+        )}
+
+        {/* CODE FOR DOCUMENT CARD DESGIN */}
+
+        <div className="blog_sect">
+          <div className="row">
+            {propertyDocument &&
+              propertyDocument.map((doc, index) => (
+                <div className="col-md-4">
+                  <div className="item card-container" key={index}>
+                    <div className="card-image">
+                      {doc.mediaType === "pdf" ? (
+                        <iframe
+                          title="PDF Viewer"
+                          src={doc.documentUrl}
+                          style={{
+                            width: "100%",
+                            aspectRatio: "3/2"
+                          }}
+                        ></iframe>
+                      ) : (
+                        <img src={doc.documentUrl || "https://via.placeholder.com/150"} />
+                      )}
+
+                    </div>
+                    <div className="card-body">
+                      <h3>
+                        {doc.idType}
+                      </h3>
+                      <p className="card-subtitle">
+                        {doc.idNumber}
+                      </p>
+                      <div className="card-author">
+
+                        <div onClick={() => deletePropertyDocument(doc.id)} className="learn-more pointer">
+                          Delete
+                        </div>
+
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+          </div>
+        </div>
       </div>
     </div>
   );
