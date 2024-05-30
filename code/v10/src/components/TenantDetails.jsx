05839d0a6070467d2bfb9c55fa05eb510827e377
atulmani <atul@hyperclouddigital.com>
2024-05-30 21:17:02 +0530

    tenant details document addition

--- code/v10/src/components/TenantDetails.jsx
+++ code/v10/src/components/TenantDetails.jsx
@@ -111,6 +111,44 @@ const TenantDetails = () => {
     setHandleMoreOptionsClick(false);
   };
   // 9 dots controls
+
+
+  const { addDocument, updateDocument, deleteDocument, error } = useFirestore("docs");
+  const [showAIForm, setShowAIForm] = useState(false);
+  const handleShowAIForm = () => setShowAIForm(!showAIForm);
+  const [isUploading, setIsUploading] = useState(false);
+  const [selectedIdType, setSelectedIdType] = useState("");
+  const [idNumber, setIdNumber] = useState("");
+  const handleRadioChange = (event) => setSelectedIdType(event.target.value);
+  const handleIdNumberChange = (event) => setIdNumber(event.target.value);
+
+  const addTenantDocuments = async () => {
+    if (!selectedIdType || !idNumber) {
+      alert("All fields are required!");
+      return;
+    }
+
+    try {
+      setIsUploading(true);
+      const docRef = await addDocument({
+        status: "",
+        propertyId: propertyDocumentId,
+        documentUrl: "",
+        idType: selectedIdType,
+        idNumber: idNumber,
+        mediaType: "",
+      });
+      setSelectedIdType("");
+      setIdNumber("");
+      setIsUploading(false);
+      setNewDocId(docRef.id);
+    } catch (error) {
+      console.error("Error adding document:", error);
+      setIsUploading(false);
+    }
+  };
+
+
   return (
     <div className="tenant_detail_pg">
       <div className="top_header_pg pg_bg">
@@ -172,7 +210,7 @@ const TenantDetails = () => {
                       </div>
                     ) : (
                       <>
-                        <img
+                        <img alt="="
                           src={imageURL || "/assets/img/upload_img_small.png"}
                         />
                         <label htmlFor="upload" className="upload_img">
@@ -567,6 +605,182 @@ const TenantDetails = () => {
                 </div>
               </div>
             </div>
+
+            <hr />
+            <div className="pg_header d-flex align-items-center justify-content-between">
+              <div className="left">
+                <h2 className="m22 mb-1">Documents</h2>
+                <h4 className="r16 light_black">
+                  Add new, show and download existing document{" "}
+                </h4>
+              </div>
+              <div className="right">
+                {!showAIForm && (
+                  <div className="theme_btn btn_fill" onClick={handleShowAIForm}>
+                    Add document
+                  </div>
+                )}
+              </div>
+            </div>
+            {showAIForm && (
+              <>
+                <div className="vg22"></div>
+                <section className="my_big_card">
+                  <h2 className="card_title">Select any one document ID</h2>
+                  <div className="aai_form">
+                    <div className="row" style={{ rowGap: "18px" }}>
+                      <div className="col-md-12">
+                        <div className="form_field">
+                          <div className="field_box theme_radio_new">
+                            <div className="theme_radio_container">
+                              <div className="radio_single">
+                                <input
+                                  type="radio"
+                                  name="aai_type"
+                                  id="indexii"
+                                  value="Index II"
+                                  onChange={handleRadioChange}
+                                  checked={selectedIdType === "Index II"}
+                                />
+                                <label htmlFor="indexii">Index II</label>
+                              </div>
+                              <div className="radio_single">
+                                <input
+                                  type="radio"
+                                  name="aai_type"
+                                  id="rentagreement"
+                                  value="Rent Agreement"
+                                  onChange={handleRadioChange}
+                                  checked={selectedIdType === "Rent Agreement"}
+                                />
+                                <label htmlFor="rentagreement">
+                                  Rent Agreement
+                                </label>
+                              </div>
+                              <div className="radio_single">
+                                <input
+                                  type="radio"
+                                  name="aai_type"
+                                  id="layout"
+                                  value="Layout"
+                                  onChange={handleRadioChange}
+                                  checked={selectedIdType === "Layout"}
+                                />
+                                <label htmlFor="layout">Layout</label>
+                              </div>
+                              <div className="radio_single">
+                                <input
+                                  type="radio"
+                                  name="aai_type"
+                                  id="blueprint"
+                                  value="Blue Print"
+                                  onChange={handleRadioChange}
+                                  checked={selectedIdType === "Blue Print"}
+                                />
+                                <label htmlFor="blueprint">Blue Print</label>
+                              </div>
+                              <div className="radio_single">
+                                <input
+                                  type="radio"
+                                  name="aai_type"
+                                  id="aadhar"
+                                  value="Aadhar"
+                                  onChange={handleRadioChange}
+                                  checked={selectedIdType === "Aadhar"}
+                                />
+                                <label htmlFor="aadhar">Aadhar</label>
+                              </div>
+                              <div className="radio_single">
+                                <input
+                                  type="radio"
+                                  name="aai_type"
+                                  id="passport"
+                                  value="Passport"
+                                  onChange={handleRadioChange}
+                                  checked={selectedIdType === "Passport"}
+                                />
+                                <label htmlFor="passport">Passport</label>
+                              </div>
+                              <div className="radio_single">
+                                <input
+                                  type="radio"
+                                  name="aai_type"
+                                  id="voterid"
+                                  value="Voter Id"
+                                  onChange={handleRadioChange}
+                                  checked={selectedIdType === "Voter Id"}
+                                />
+                                <label htmlFor="voterid">Voter Id</label>
+                              </div>
+                              <div className="radio_single">
+                                <input
+                                  type="radio"
+                                  name="aai_type"
+                                  id="drivinglicense"
+                                  value="Driving Licence"
+                                  onChange={handleRadioChange}
+                                  checked={selectedIdType === "Driving Licence"}
+                                />
+                                <label htmlFor="drivinglicense">
+                                  Driving Licence
+                                </label>
+                              </div>
+                              <div className="radio_single">
+                                <input
+                                  type="radio"
+                                  name="aai_type"
+                                  id="pancard"
+                                  value="Pan Card"
+                                  onChange={handleRadioChange}
+                                  checked={selectedIdType === "Pan Card"}
+                                />
+                                <label htmlFor="pancard">Pan Card</label>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+
+                      <div className="col-md-11">
+                        <div className="add_info_text">
+                          <div className="form_field">
+                            <div className="relative">
+                              <input
+                                type="text"
+                                value={idNumber}
+                                onChange={handleIdNumberChange}
+                                placeholder="Enter Document Id"
+                                required
+                              />
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row mt-3">
+                    <div className="col-sm-1">
+                      <div
+                        className="theme_btn btn_border text-center"
+                        onClick={isUploading ? null : handleShowAIForm}
+                      >
+                        Cancel
+                      </div>
+                    </div>
+                    <div className="col-sm-3">
+                      <div
+                        className={`theme_btn btn_fill text-center ${isUploading ? "disabled" : ""
+                          }`}
+                        onClick={isUploading ? null : addTenantDocuments}
+                      >
+                        {isUploading ? "Uploading..." : "Save"}
+                      </div>
+                    </div>
+                  </div>
+                </section>
+              </>
+            )}
+
             {!editingField && user && user.role === "admin" && (
               <>
                 <div className="vg22"></div>
