719c61d89999b8388522cbfd2a4e42f0404b9842
atulmani <atul@hyperclouddigital.com>
2024-11-03 19:23:11 +0530

    Import data

--- code/v1.3/src/pdpages/util/PGImportExcel.jsx
+++ code/v1.3/src/pdpages/util/PGImportExcel.jsx
@@ -6,15 +6,27 @@ import { useFirestore } from "../../hooks/useFirestore";
 // component
 import ImportExcel from "../../components/utils/ImportExcel";
 import { useCollection } from "../../hooks/useCollection";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 import { useExcelImport } from "./useExcelImport";
 const PGExportExcel = () => {
+    const { user } = useAuthContext();
     // Scroll to the top of the page whenever the location changes start
     const location = useLocation();
     const { addDocument, updateDocument, error } = useFirestore("agent-propdial");
     const { isPending, error: download, documents } = useCollection("agent-propdial");
     const { exportExcel } = useExcelImport()
-
+    const [count, setCount] = useState(0);
+    const [totalCount, setTotalCount] = useState(0);
+    const agentDataTemplate = [{
+        agentName: '',
+        agentCompnayName: '',
+        agentPhone: '',
+        agentEmail: '',
+        state: '',
+        city: '',
+        locality: ''
+    }]
     useEffect(() => {
         window.scrollTo(0, 0);
     }, [location]);
@@ -22,33 +34,66 @@ const PGExportExcel = () => {
     const navigate = useNavigate();
 
     const setJSON = (json) => {
-        console.log("json: ", json)
+        console.log("json: ", json, user)
+        let record = {};
+        setTotalCount(json.length);
+        console.log('_totalCount', json.length)
         json.forEach(element => {
+
+            let _locality = element.locality
+            let _localityList = _locality.split(';')
+            console.log(_localityList, _locality)
+            record = {
+                ...element,
+                locality: _locality === '' ? [] : _localityList,
+                country: 'India',
+                createdAt: new Date(),
+                createdBy: user && user.uid
+
+            }
+            console.log('record', record)
+            setCount(count + 1);
+            console.log(count)
             // if (element.documentID === '')
-            addDocument(element)
+            addDocument(record)
             // else{
             // updateDocument(element.documentID,{})
             // }
 
         });
+        console.log('upload completed !!')
 
     }
     function downloadExcle() {
-        exportExcel(documents, 'user_propdial');
+        // let singleRow = [documents[0]];
+        exportExcel(documents, 'Agentdetails_Excel');
+    }
+    function downloadTemplate() {
+        let singleRow = [documents[0]];
+        exportExcel(agentDataTemplate, 'template');
     }
     return (
         <>
             {/* Agent Excel import/export - Start */}
             <section>
                 <div>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
                     <h3>Agent Data Collection</h3>
                     <div>
-                        <button onClick={downloadExcle}> Download Template for Agent</button>
+                        <button onClick={downloadTemplate}> Download Template for Agent</button>
+                    </div>
+                    <div>
+                        <button onClick={downloadExcle}> Export Data for Agent</button>
                     </div>
                     <br></br>
                     <div>
-                        <h6>Import Data into Firebase Collection</h6>
+                        <h6>Import Data into Firebase Collection123</h6>
                         <ImportExcel setJSON={setJSON}></ImportExcel>
+                        <h6> Total Record : {totalCount} </h6>
+                        <h6> Record processed : {count} </h6>
                     </div>
 
                 </div>
