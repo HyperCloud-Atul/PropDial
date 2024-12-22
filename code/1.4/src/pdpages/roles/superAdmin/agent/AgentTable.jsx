378dea046c3195df58d450744da14c6a1aa8e25f
Sanskar Solanki <136140403+sanskarsolankigithub@users.noreply.github.com>
2024-12-22 12:28:10 +0530

    agent_table

--- code/1.4/src/pdpages/roles/superAdmin/agent/AgentTable.jsx
+++ code/1.4/src/pdpages/roles/superAdmin/agent/AgentTable.jsx
@@ -34,6 +34,17 @@ const AgentTable = ({ agentDoc }) => {
   };
   // Handle text copy and display "Copied!" indicator end
 
+    // code for open modal for selected Agent start
+    const [selectedAgent, setSelectedAgent] = useState(null);
+    const [showAgentModal, setShowAgentModal] = useState(false);
+  
+    const handleAgentModalClose = () => setShowAgentModal(false);
+    const handleShowAgentModal = (doc) => {
+      setSelectedAgent(doc);
+      setShowAgentModal(true);
+    };
+    // code for open modal for selected Agent end
+
   const columns = useMemo(() => {
     if (!dbUserState) return []; // Return an empty array if dbUserState is not available
 
@@ -42,20 +53,53 @@ const AgentTable = ({ agentDoc }) => {
         Header: "S.No",
         accessor: (row, i) => i + 1,
         id: "serialNumber",
-        Cell: ({ row }) => row.index + 1,
+        Cell: ({ row }) =>
+           (
+            <span className="min_width_inherit">{ row.index + 1}</span>
+           ),
         disableFilters: true,
+      },
+      {
+        Header: "Action",
+        accessor: "id",
+        disableFilters: true,
+        Cell: ({ row }) => (
+          <div className="d-flex align-items-center min_width_inherit">
+            <span
+              className="material-symbols-outlined click_icon pointer"
+              style={{ fontSize: "20px", marginRight: "8px" }}
+              onClick={() => handleShowAgentModal(row.original)}
+            >
+              visibility
+            </span>
+            {user && (user.role === "admin" || user.role === "superAdmin") && (
+              <Link to={`/edit-agent/${row.original.id}`}>
+                <span
+                  className="material-symbols-outlined click_icon pointer"
+                  style={{ fontSize: "20px" }}
+                >
+                  border_color
+                </span>
+              </Link>
+            )}
+          </div>
+        ),
       },
       {
         Header: "Name",
         accessor: "agentName",
+        Cell: ({ value }) => (
+            <div className="name">{value}</div>
+          ),
       },
       {
         Header: "Phone Number",
+        className: "phone_header",
         accessor: "agentPhone",
         Cell: ({ value, row }) => {
           const uniqueId = `phone-${row.index}`;
           return (
-            <div className="copy_value">
+            <div className="copy_value phone_number_width">
               <div onClick={() => handleCopy(value, uniqueId)}>
                 {value.replace(/(\d{2})(\d{5})(\d{5})/, "+$1 $2-$3")}
               </div>
@@ -65,6 +109,7 @@ const AgentTable = ({ agentDoc }) => {
             </div>
           );
         },
+   
       },
       {
         Header: "Email",
@@ -116,6 +161,9 @@ const AgentTable = ({ agentDoc }) => {
       {
         Header: "Company",
         accessor: "agentCompnayName",
+        Cell: ({ value }) => (
+            <div className="company_name">{value}</div>
+          ),
       },
       {
         Header: "City",
@@ -141,7 +189,7 @@ const AgentTable = ({ agentDoc }) => {
         Header: "Added At",
         accessor: "createdAt",
         Cell: ({ value }) => (
-          <div className="">{format(value.toDate(), "dd-MMM-yy hh:mm a")}</div>
+          <div className="createdat">{format(value.toDate(), "dd-MMM-yy hh:mm a")}</div>
         ),
       },
     ];
@@ -150,6 +198,12 @@ const AgentTable = ({ agentDoc }) => {
   return (
     <div className="user-single-table table_filter_hide mt-3">
       <ReactTable tableColumns={columns} tableData={agentDoc} />
+      <AgentDetailModal
+        show={showAgentModal}
+        handleClose={handleAgentModalClose}
+        selectedAgent={selectedAgent}
+        user={user}
+      />    
     </div>
   );
 };
