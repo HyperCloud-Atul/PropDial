cbb0eba7c89f10141ecaab9f6e6364f0ae745ebb
Gurjap Singh <nightmare7468@gmail.com>
2025-07-29 22:11:00 +0530

    sorted_blogs

--- code/v1.8/src/pdpages/blogs/PGBlogs.jsx
+++ code/v1.8/src/pdpages/blogs/PGBlogs.jsx
@@ -23,7 +23,6 @@ const PGBlogs1 = () => {
       .replace(/[^a-z0-9\s-]/g, "")
       .replace(/\s+/g, "-");
   };
-
   return (
     <div className="blog_page blog_page_css">
       {/* Hero Section */}
@@ -41,47 +40,62 @@ const PGBlogs1 = () => {
           {!blogDoc && <p>Loading blogs...</p>}
           <div className="blog_inner relative">
             {blogDoc &&
-              blogDoc.map((blog) => {
-                const slug = generateSlug(blog.slug || blog.title);
-                return (
-                  <div key={blog.id} className="item card-container">
-                    <div className="card-image">
-                      <Link to={`/blog/${slug}`}>
-                        <img src={blog.image.url} alt={blog.title} />
-                        <div className="published_date">
-                          {blog.updatedAt?.toDate
-                            ? format(blog.updatedAt.toDate(), "dd-MMM-yyyy")
-                            : format(blog.createdAt.toDate(), "dd-MMM-yyyy")}
-                        </div>
+              [...blogDoc]
+                .sort((a, b) => {
+                  // Use updatedAt if available, otherwise fallback to createdAt
+                  const dateA = a.updatedAt?.toDate
+                    ? a.updatedAt.toDate()
+                    : a.createdAt.toDate();
+                  const dateB = b.updatedAt?.toDate
+                    ? b.updatedAt.toDate()
+                    : b.createdAt.toDate();
+                  return dateB - dateA; // Descending order
+                })
+                .map((blog) => {
+                  const slug = generateSlug(blog.slug || blog.title);
+
+                  return (
+                    <div key={blog.id} className="item card-container">
+                      <div className="card-image">
+                        <Link to={`/blog/${slug}`}>
+                          <img src={blog.image.url} alt={blog.title} />
+                          <div className="published_date">
+                            {blog.updatedAt?.toDate
+                              ? format(blog.updatedAt.toDate(), "dd-MMM-yyyy")
+                              : format(blog.createdAt.toDate(), "dd-MMM-yyyy")}
+                          </div>
+                        </Link>
+
+                        {user &&
+                          (user.role === "frontdesk" ||
+                            user.role === "admin" ||
+                            user.role === "superAdmin") && (
+                            <div className="author-right">
+                              <Link
+                                className="edit"
+                                to={`/blog-edit/${blog.id}`}
+                              >
+                                Edit
+                              </Link>
+                            </div>
+                          )}
+                      </div>
+
+                      <Link className="card-body" to={`/blog/${slug}`}>
+                        <h3>{blog.title}</h3>
+                        <p className="card-subtitle">{blog.subTitle}</p>
                       </Link>
 
-                      {user &&
-                        (user.role === "frontdesk" ||
-                          user.role === "admin" ||
-                          user.role === "superAdmin") && (
-                          <div className="author-right">
-                            <Link className="edit" to={`/blog-edit/${blog.id}`}>
-                              Edit
-                            </Link>
-                          </div>
-                        )}
-                    </div>
-
-                    <Link className="card-body" to={`/blog/${slug}`}>
-                      <h3>{blog.title}</h3>
-                      <p className="card-subtitle">{blog.subTitle}</p>
-                    </Link>
-
-                    <div className="card-author">
-                      <div className="author-left">
-                        <Link className="read-more" to={`/blog/${slug}`}>
-                          Read More <span className="arrow">&rarr;</span>
-                        </Link>
+                      <div className="card-author">
+                        <div className="author-left">
+                          <Link className="read-more" to={`/blog/${slug}`}>
+                            Read More <span className="arrow">&rarr;</span>
+                          </Link>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                );
-              })}
+                  );
+                })}
           </div>
         </div>
       </section>
